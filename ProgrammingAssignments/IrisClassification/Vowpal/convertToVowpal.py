f68a82fefac681890592cd6eb21c27377389d1c2
Carl Cortright <ckcortright@gmail.com>
2016-09-12 18:45:22 -0600

    Fixed vowpal wabbit datasets and completed report

--- ProgrammingAssignments/IrisClassification/Vowpal/convertToVowpal.py
+++ ProgrammingAssignments/IrisClassification/Vowpal/convertToVowpal.py
@@ -8,37 +8,65 @@
 ################################################################################
 import csv
 import random
+import sys
 
-# Open the relevant files
-vowpal_output = open("vowpal.iris.test.data.vw", "r+")
-iris = open("iris.test.data", "rt")
-iris_csv = csv.reader(iris, delimiter=",")
+#
+# Converts a file to the vowpal format
+#
+def convertToVowpal(filename):
 
-data_entries = []
+    # Open the relevant files
+    iris = open(filename, "r+")
+    iris_csv = csv.reader(iris, delimiter=",")
 
-# Add the data to the vowpal_output file in the correct format
-for row in iris_csv:
-    label = 0
-    if(row[4] == "Iris-setosa"):
-        label = 1
-    elif(row[4] == "Iris-versicolor"):
-        label = 2
-    elif(row[4] == "Iris-virginica"):
-        label = 3
-    # Generate the data entry
-    new_data_entry = ""
-    new_data_entry += str(label) + " |"
-    new_data_entry += " SepalLength:" + str(row[0])
-    new_data_entry += " SepalWidth:" + str(row[1])
-    new_data_entry += " PedalLength:" + str(row[2])
-    new_data_entry += " PedalWidth:" + str(row[3])
-    new_data_entry += "\n"
-    data_entries.append(new_data_entry)
+    data_entries = []
 
-random.shuffle(data_entries)
-for entry in data_entries:
-    vowpal_output.write(entry)
+    # Add the data to the vowpal_output file in the correct format
+    for row in iris_csv:
+        label = 0
+        if(row[4] == "Iris-setosa"):
+            label = 1
+        elif(row[4] == "Iris-versicolor"):
+            label = 2
+        elif(row[4] == "Iris-virginica"):
+            label = 3
+        # Generate the data entry
+        new_data_entry = ""
+        new_data_entry += str(label) + " | "
+        new_data_entry += str(row[0]) + ":1 "
+        new_data_entry += str(row[1]) + ":1 "
+        new_data_entry += str(row[2]) + ":1 "
+        new_data_entry += str(row[3]) + ":1 "
+        new_data_entry += "\n"
+        data_entries.append(new_data_entry)
+
+    random.shuffle(data_entries)
+    for entry in data_entries:
+        iris.write(entry)
+
+    iris.close()
+
+#
+# Shuffles the dataset based on a ratio training:test
+#
+def shuffleData(training, test):
+    iris_data = open("iris.data", "r+")
+    iris_training = open("iris.training.data", "w")
+    iris_test = open("iris.test.data", "w")
+
+    all_data = iris_data.readlines()
+    random.shuffle(all_data)
+    all_data_len = len(all_data)
 
 
-iris.close()
-vowpal_output.close()
+    split_point = int( all_data_len * ( float(training) / (training + test)))
+    for i in range(0, split_point):
+        iris_training.write(all_data[i])
+    for j in range(split_point, all_data_len - 1):
+        iris_test.write(all_data[j])
+
+    iris_data.close()
+    iris_training.close()
+    iris_test.close()
+
+shuffleData(int(sys.argv[1]), int(sys.argv[2]))
--- ProgrammingAssignments/IrisClassification/Vowpal/run.sh
+++ ProgrammingAssignments/IrisClassification/Vowpal/run.sh
@@ -7,18 +7,22 @@
 # Copyright 2016 Carl Cortright
 ################################################################################
 
+# Randomize the dataset
+
 # Train the model
 echo "Training vw model..."
 rm iris.model
-vw -f iris.model --passes=10 --cache_file=iris.cache --kill_cache  --oaa 3 --loss_function=hinge < training_iris.data
+rm predictions.txt
+vw -f iris.model --passes=10 --cache_file=iris.cache --kill_cache  --oaa 3 --loss_function=hinge < iris.training.data
+
 # Test the model
 echo
 echo
 echo "Testing model..."
-vw -i iris.model --cache_file=test.cache -t -p ./predictions.txt < testing_iris.data
+vw -i iris.model --cache_file=test.cache -t -p ./predictions.txt < iris.test.data
 
 # Calculate the accuracy of the model
 echo
 echo
 echo "Calculating Accuracy..."
-python calcAccuracy.py vowpal.iris.test.data.vw predictions.txt
+python calcAccuracy.py iris.test.data predictions.txt
